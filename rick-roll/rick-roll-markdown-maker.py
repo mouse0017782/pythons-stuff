ef09e00c995001851915ded691fe27df9cc8edf0
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-16 09:01:35 -0500

    Update rick-roll-markdown-maker.py

--- rick-roll/rick-roll-markdown-maker.py
+++ rick-roll/rick-roll-markdown-maker.py
@@ -2,5 +2,5 @@ file = open("rick-roll.txt", "x")
 file = open("rick-roll.txt", "w")
 text = "insert your text here"
 file.write("[" + text + "]" + "(https://youtu.be/dQw4w9WgXcQ?si=nelBnas0wSu0yxWJ)")
-print(file)
+print("[" + text + "]" + "(https://youtu.be/dQw4w9WgXcQ?si=nelBnas0wSu0yxWJ)")
 file.close()
